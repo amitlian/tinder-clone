7667eef602d3337f17fd8b028fdcf5be3090dddc
Amit <lianbadhon@gmail.com>
2021-02-20 19:31:12 +0600

    first commit

---
+++ src/TinderCardFiles/TinderCard.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import "../TinderCardFiles/TinderCard.css"
+
+function TinderCard() {
+    const [people,setPeople] = useState([])
+    return (
+        <div className="tinderClass">
+            
+        </div>
+    )
+}
+
+export default TinderCard
